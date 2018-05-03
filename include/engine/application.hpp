de22f1033f0ed4e870e2fac82ef10fc202c3c021
Pedja Trifunov <trifunov.pedja@gmail.com>
2018-05-03 08:43:57 +0200

    Added new api to application allowing to retrieve scene with it's uuid, and added support for threads that auto join after finishing with their work

--- include/engine/application.hpp
+++ include/engine/application.hpp
@@ -1,31 +1,54 @@
 #ifndef APPLICATION_HPP_PJTWXWVJ
 #define APPLICATION_HPP_PJTWXWVJ
 
+#include <map>
+#include <thread>
+#include <utility>
 #include "engine/id.hpp"
+#include "engine/thread_finished_event.hpp"
 
 
-namespace rg
-{
+namespace rg {
 namespace engine
 {
 	class window;
-	class event;
 	class log;
 	class scene;
+	class event;
 
 namespace application
 {
-	
+
+	extern std::map<uuid, std::thread> threads_;
+
 	void init(int argc, char** argv);
-	void add_window(window* w);	
+	void add_window(window* w);
 	void add_scene(scene* s);
 	bool scene_exist(const uuid& id);
+	scene* scene(const uuid& id);
 	void remove_window(int id);
 	void remove_scene(const uuid& id);
 	void post_event(const event& e);
 	void exec(void);
-	log* logger(void);	
+	log* logger(void);
+	template<typename Fn, typename... Args>
+	void create_thread(Fn&& f, Args&&... args);
 
+
+
+	template<typename Fn, typename... Args>
+	void on_thread(uuid th_id, Fn&& f, Args&&... a)
+	{
+		f(std::forward<Args>(a)...);
+		post_event(thread_finished_event(uuids::null_id,th_id));
+	}
+
+	template<typename Fn, typename... Args>
+	void create_thread(Fn&& f, Args&&... args)
+	{
+		uuid id = uuids::random_id();
+		threads_.insert(std::make_pair(id, std::thread(on_thread<std::decay_t<Fn>, std::decay_t<Args>...>, id, std::forward<Fn>(f),std::forward<Args>(args)...)));
+	}
 }
 }
 }
