9bdbc69c228d034526ba974dd9e296491d2ab1a5
Pedja Trifunov <trifunov.pedja@gmail.com>
2018-05-30 12:03:36 +0200

    Moved game_controler from scenes to objects directory

--- include/game/objects/game_controler.hpp
+++ include/game/objects/game_controler.hpp
@@ -1,31 +1,41 @@
 #ifndef GAME_CONTROLER_HPP_NJBXIHKJ
 #define GAME_CONTROLER_HPP_NJBXIHKJ
 
-
-
 #include <vector>
 #include "engine/game_object.hpp"
 #include "engine/color.hpp"
 #include "engine/timer.hpp"
+#include "engine/model_loader.hpp"
+#include "engine/texture_loader.hpp"
+#include "game/objects/ship.hpp"
+#include "game/objects/chicken.hpp"
 
 class game_controler  : public rg::engine::game_object
 {
 	public:
-		game_controler(void);			
+		game_controler(int wave_number = 1);
 		game_controler(const game_controler& c);
-		
+
 		rg::engine::game_object* copy(void);
 		void render(void);
 		bool handle_events(const rg::engine::event& e);
 		rg::engine::box box_mash(void);
-
+		
+		void load_models(rg::engine::model_loader& ml);
 
 		void on_frame(void);
 		~game_controler(void) = default;
+
+
+		friend void init_level_scene(game_controler& gc);
 	private:
 		std::unique_ptr<rg::engine::timer> t_;
 		rg::engine::uuid back_scene_;
 		rg::engine::uuid camid_;
+		int wave_number_;
+		bool game_started_;
+		ship* ship_;
+		chicken* chicken_;
 };
 
 #endif /* end of include guard: GAME_CONTROLER_HPP_NJBXIHKJ */
--- src/game/objects/game_controler.cpp
+++ src/game/objects/game_controler.cpp
@@ -2,7 +2,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include "engine/log.hpp"
 #include "engine/application.hpp"
-#include "game/scenes/game_controler.hpp"
 #include "engine/event.hpp"
 #include "engine/application.hpp"
 #include "engine/scene.hpp"
@@ -15,19 +14,24 @@
 #include "engine/user_event.hpp"
 #include "engine/pause_event.hpp"
 #include "engine/support/bundle.hpp"
+#include "game/objects/game_controler.hpp"
 
 using log_ = rg::engine::log;
 namespace app = rg::engine::application;
 
-rg::engine::color gen_random_color(void);
+void init_level_scene(game_controler& gc);
 
-game_controler::game_controler(void) 
-	: t_(nullptr) 
+game_controler::game_controler(int wave_number) 
+	:	t_(nullptr),
+		wave_number_{wave_number},
+		game_started_{false}
 {}
 
 game_controler::game_controler(const game_controler& o)
 	:	game_object(o),
-		t_(new rg::engine::timer(*o.t_))
+		t_(new rg::engine::timer(*o.t_)),
+		wave_number_{o.wave_number_},
+		game_started_{o.game_started_}
 {}
 
 void game_controler::on_frame(void)
@@ -55,12 +59,23 @@ bool game_controler::handle_events(const rg::engine::event& e)
 						(*app::logger()) << log_::priority::info << "Tick from timer in scene with id: " << scene()->id() << log_::end_log;
 					#endif
 
+					if(game_started_)
+					{
+						/*Game tick logic here*/
+					}
+
 				}
 				else if(e_.code() == rg::engine::user_event_code<rg::engine::resume_event>::value)
 				{
-					t_ = std::make_unique<rg::engine::timer>(this->scene()->id(), id(),  50, true);
+					t_ = std::make_unique<rg::engine::timer>(this->scene()->id(), rg::engine::uuids::null_id,  50, true);
 					t_->start();
 					(*app::logger()) << log_::priority::info << "Timer started in game object within scene id: " << scene()->id() << log_::end_log;
+
+					(*app::logger()) << log_::priority::info << "Initialazing level scene with objects" << log_::end_log;
+					init_level_scene(*this);
+
+					(*app::logger()) << log_::priority::info << "Game starting" << log_::end_log;
+					game_started_ = true;
 				}
 				else if(e_.code() == rg::engine::user_event_code<rg::engine::pause_event>::value)
 				{
@@ -93,6 +108,7 @@ bool game_controler::handle_events(const rg::engine::event& e)
 
 		if(SDLK_ESCAPE == se.key.keysym.sym)
 		{
+			(*app::logger()) << log_::priority::info << "You closed a level" << log_::end_log;
 			auto win = scene()->windows()[0];
 			win->attach_scene(back_scene_, rg::engine::support::make_bundle_event(back_scene_, rg::engine::uuids::null_id, scene()->id()));
 			win->attach_camera(camid_, [](int w, int h){return std::make_tuple(0,0, w, h);});
@@ -100,6 +116,21 @@ bool game_controler::handle_events(const rg::engine::event& e)
 		}
 	}
 	return false;
+}
+
+void game_controler::load_models(rg::engine::model_loader& ml)
+{
+	ship_ = new ship(ml);
+	ship_->rtransform(glm::translate(glm::mat4(1), glm::vec3{0, 0, 4.5}));
+	ship_->rtransform(glm::rotate(glm::mat4(1), static_cast<float>(M_PI), glm::vec3{0,1,0}));
+	ship_->rtransform(glm::scale(glm::mat4(1), 0.001f*glm::vec3{1,1,1}));
+
+	chicken_ = new chicken(ml.load("./materials/chicken/model.obj").second, this);
+	chicken_->ltransform(glm::scale(glm::mat4(1), 0.04f*glm::vec3{1,1,1}));
+	chicken_->ltransform(glm::rotate(glm::mat4(1), static_cast<float>(M_PI), glm::vec3{0,0,1}));
+	chicken_->ltransform(glm::rotate(glm::mat4(1), static_cast<float>(-M_PI/4), glm::vec3{1,0,0}));
+	chicken_->ltransform(glm::rotate(glm::mat4(1), static_cast<float>(M_PI), glm::vec3{0,1,0}));
+
 }
 
 rg::engine::game_object* game_controler::copy(void)
@@ -110,4 +141,12 @@ rg::engine::game_object* game_controler::copy(void)
 rg::engine::box game_controler::box_mash(void)
 {
 	return rg::engine::box();
+}
+
+
+void init_level_scene(game_controler& gc)
+{
+	gc.scene()->add_object(gc.ship_);	
+	auto t = gc.chicken_->copy();
+	gc.scene()->add_object(t);
 }
