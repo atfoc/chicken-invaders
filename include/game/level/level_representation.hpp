a40c89258ab8f9d6d3dec620139ccba53033d082
Pedja Trifunov <trifunov.pedja@gmail.com>
2018-06-11 19:25:12 +0200

    Fixed some mistakes made by using sed for refactoring

--- include/game/level/level_representation.hpp
+++ include/game/level/level_representation.hpp
@@ -1,46 +1,37 @@
 #ifndef LEVEL_REPRESENTATION_HPP_G7JWQ3IS
 #define LEVEL_REPRESENTATION_HPP_G7JWQ3IS
 
-/**
- * @briefthis is the function that allows you to
- *copy an object polymorphicly
- *
- * @returncopied object as gameobject
- */
+
 #include "engine/game_object.hpp"
 #include "engine/color.hpp"
 
 
-/**
- * @briefthis is the function that allows you to
- *copy an object polymorphicly
- *
- * @returncopied object as gameobject
- */
+
 class level_representation : public rg::engine::game_object
 {
 	public:
 		level_representation(unsigned model, unsigned texture);	
 		level_representation(const level_representation& l) = default;
 
-/**
- * @brief function that is called by the system for object to render
- */
+		/**
+		 * @brief function that is called by the system for object to render
+		 */
 
 		void render(void);
-/**
- * @briefthis is the function that allows you to
- *copy an object polymorphicly
- *
- * @returncopied object as gameobject
- */
+		/**
+		 * @briefthis is the function that allows you to
+		 *copy an object polymorphicly
+		 *
+		 * @returncopied object as gameobject
+		 */
 		rg::engine::game_object* copy(void);
-n/**
- * @briefthis function returns box representation that is used for collision
- *detection
- *
- * @returnbox that actually is rectangle
- */
+
+		/**
+		 * @briefthis function returns box representation that is used for collision
+		 *detection
+		 *
+		 * @returnbox that actually is rectangle
+		 */
 		rg::engine::box box_mash(void);
 
 		void on_tick(void);
--- include/game/scenes/level_loader.hpp
+++ include/game/scenes/level_loader.hpp
@@ -2,56 +2,45 @@
 #define LEVEL_LOADER_HPP_ILMR28BJ
 
 #include <memory>
-/**
- * @briefthis is the function that allows you to
- *copy an object polymorphicly
- *
- * @returncopied object as gameobject
- */
 #include "engine/game_object.hpp"
 #include "engine/timer.hpp"
 #include "engine/support/bundle.hpp"
 #include "engine/id.hpp"
 
-/**
- * @briefthis is the function that allows you to
- *copy an object polymorphicly
- *
- * @returncopied object as gameobject
- */
 class level_loader : public rg::engine::game_object
 {
 	public:
 		level_loader(void);
 		level_loader(const level_loader& l);
 
-/**
- * @briefthis is the function that allows you to
- *copy an object polymorphicly
- *
- * @returncopied object as gameobject
- */
+		/**
+		 * @briefthis is the function that allows you to
+		 *copy an object polymorphicly
+		 *
+		 * @returncopied object as gameobject
+		 */
 		rg::engine::game_object* copy(void);	
-n/**
- * @briefthis function returns box representation that is used for collision
- *detection
- *
- * @returnbox that actually is rectangle
- */
+
+		/**
+		 * @briefthis function returns box representation that is used for collision
+		 *detection
+		 *
+		 * @returnbox that actually is rectangle
+		 */
 		rg::engine::box box_mash(void);
 
-/**
- * @Brief function that is called by the system for handling events
- *
- * @param e event to be handled
- *
- * @return true if the events was handled else false
- */
+		/**
+		 * @Brief function that is called by the system for handling events
+		 *
+		 * @param e event to be handled
+		 *
+		 * @return true if the events was handled else false
+		 */
 		bool handle_events(const rg::engine::event& e);
-/**
- * @brief function that is called by the system for object to render
- */
 
+		/**
+		 * @brief function that is called by the system for object to render
+		 */
 		void render(void);
 
 		~level_loader(void) = default;
